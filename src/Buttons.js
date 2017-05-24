fae2da702aa6bd16485467cc197374432bcd8407
Lizette Romero <lmromero@anderson.edu>
2017-05-24 10:32:23 -0400

    Remove Input field

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,20 +2,9 @@ import React, { Component } from 'react'
 import './Buttons.css'
 
 class Buttons extends Component{
-addThing(ev){
-    ev.preventDefault()
-    console.log(this)
-}
 
  render() {
-  return (
-      <div>
-      <button className="logout">Sign Out</button>
-      <form onSubmit = {this.addThing}>
-      <input className="type" type= "text" placeholder="Enter a thing."/>
-      </form>
-      </div>
-     );
+  return <button className="logout">Sign Out</button>
   }
 }
 
