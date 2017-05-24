1138bc791f66178f3e4387ba414bc633fd0e8232
Lizette Romero <lmromero@anderson.edu>
2017-05-24 10:29:25 -0400

    make addThingButton Work

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import './Buttons.css'
 
 class Buttons extends Component{
+addThing(ev){
+    ev.preventDefault()
+    console.log(this)
+}
+
  render() {
   return (
       <div>
       <button className="logout">Sign Out</button>
-      <div>
+      <form onSubmit = {this.addThing}>
       <input className="type" type= "text" placeholder="Enter a thing."/>
-      </div>
-      <button className="add-thing">Add Thing</button>
+      </form>
       </div>
      );
   }
